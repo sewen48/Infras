bd381e913acd0d245a5834341fb01d92025e8f37
Rukoyahsanni <abisoye1010@gmail.com>
2025-02-23 13:39:43 +0100

    Infrastructure deployment (jenkins build)

---
+++ install_jenkins.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install openjdk-17-jre-headless -y
+sudo apt-get install -y ca-certificates curl gnupg
+sudo apt-get update -y
+sudo apt-get install maven -y
+sudo apt-get update -y
+sudo mkdir -p /usr/share/keyrings
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | \
+  sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | \
+  sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update -y
+sudo apt-get install -y jenkins
+sudo systemctl daemon-reload
+sudo systemctl enable Jenkins
+sudo systemctl start Jenkins
+sudo systemctl status Jenkins
+sudo apt-get update -y
+sudo snap install terraform -y
+echo 'clearing screen...' && sleep 5
+clear
+echo 'jenkins is installed'
+echo 'this is the default password :' $(sudo cat /var/lib/jenkins/secrets/initialAdminPassword)
