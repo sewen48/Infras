cf7a13c957dc254785d312e48b5ec153739ef18e
Rukoyahsanni <abisoye1010@gmail.com>
2025-02-25 17:35:15 +0100

    final backend

--- MAINinfra/Main.tf
+++ MAINinfra/Main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "customvpcT-bucket"
+    bucket = "customvpct-bucket"
     key    = "infra/terraform.tfstate"
     region = "us-east-1"
   }
